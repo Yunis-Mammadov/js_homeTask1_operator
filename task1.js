a1687601f6651f8027f5b3228ff2857fc5647a54
Yunis-Mammadovm <yunismammadovv@gmail.com>
2023-04-18 11:44:00 +0400

    task 1 added

---
+++ task1.js
@@ -0,0 +1,58 @@
+// REGULAR FUNCTION
+
+
+// function number() {
+//     let num = 17;
+//     let qaliq = 0;
+//     let result = 0;
+//     if (num % 7 == 0 ) {
+//         console.log("7-yə bölünür");
+//     }else if((num % 7) <= 3){
+//         qaliq = num % 7
+//         result = num - qaliq
+//         console.log(result);
+//     }
+// }
+
+// number()
+
+
+
+// ARROW FUNCTION
+
+
+// const number = () => {
+//     let num = 17;
+//     let qaliq = 0;
+//     let result = 0;
+//     if (num % 7 == 0 ) {
+//         console.log("7-yə bölünür");
+//     }else if((num % 7) <= 3){
+//         qaliq = num % 7
+//         result = num - qaliq
+//         console.log(result);
+//     }
+// }
+
+// number()
+
+
+
+
+// ANONYM FUNCTION
+
+
+// const number = function() {
+//     let num = 17;
+//     let qaliq = 0;
+//     let result = 0;
+//     if (num % 7 == 0 ) {
+//         console.log("7-yə bölünür");
+//     }else if((num % 7) <= 3){
+//         qaliq = num % 7
+//         result = num - qaliq
+//         console.log(result);
+//     }
+// }
+
+// number()
